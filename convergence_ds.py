fixed total population calculation

convergence_ds.py
@@ -3,26 +3,32 @@ import matplotlib.pyplot as plt
 
 def convergence_ds(size, time, ds, dt, ntag,filename, data):
 
-    # # initalize arrays
-    # Nsize = int(Smax/ds) + 1
-    # Ntime = int(Tmax/dt) + 1
-    # size = np.linspace(0,Smax, Nsize)
-    # time = np.linspace(0,Tmax, Ntime)
-
     Tend = time[-1]
-    # print(Tend)
 
+    # c = 1/10
     # Analytical Solution
-    Y   = np.log(np.exp(size) - Tend)
-    phi = np.exp(-((Y-5)/1)**2) # Initial Conditions
-    sol = phi * np.exp(Y - np.log(Tend + np.exp(Y)))  # g(s) = exp(-s), mu(s) = 0
+    # Y   = 1/c * np.log(np.exp(size*c) - Tend*c)
+    # phi = np.exp(-((Y-5)/1)**2) # Initial Conditions
+    # sol = phi * np.exp(Y - np.log(Tend + np.exp(Y)))  # g(s) = exp(-s), mu(s) = 0
 
-    
+    c = 1/10
+    Y = 1/c * np.log(np.exp(size*c) - Tend*c)  # g(s) = exp(-s/10)
+    phi = np.exp(-((Y - 5) / 1) ** 2)
+    # sol = phi * np.exp(c**2 * Y) * (c*Tend + np.exp(Y))**(-c**2)  # g(s) = exp(-s/10), mu(s) = 0
+    sol = phi * np.exp( c**2 * (Y -  np.log( c*Tend + np.exp(Y) ) ) ) # g(s) = exp(-s/10), mu(s) = 0
+
+
+    # Solve for L-2 and L-max
+    Norm2 = ( ( 1 / len(size) ) * np.sum( ( data[-1,:] - sol[:] ) **2 ) ) **0.5 # L2 error.
+    NormMax = np.max( np.abs( data[-1,:] - sol[:] ) ) # L-Max error.
+
+    print('Norm 2 : ' + str(Norm2))
+    print('Norm inf : ' + str(NormMax))
 
 
     # Plot the Analytical and Numerical Solution
     plt.plot(size, sol, label='Analytical', linestyle='solid')
-    plt.plot(size, data[0,:], label='Numerical', linestyle='--')
+    plt.plot(size, data[-1,:], label='Numerical', linestyle='--')
     # plt.axvline(x=0.4, color='r', linestyle='--', label='x=1')
     plt.xlabel('Size')
     plt.ylabel('Population')
@@ -31,5 +37,3 @@ def convergence_ds(size, time, ds, dt, ntag,filename, data):
     # plt.ylim(-.2, 1.4)  # Set y-axis limits from 0 to 12
     # plt.savefig('plots/pop_plot-time' + str(n) + '-ds_'+ str(ds_index) +'.png') # Save the plot
     plt.show()
-
-    # Ntrue = 

function_upwind.py
@@ -7,7 +7,7 @@
 import numpy as np # Numpy for numpy
 import matplotlib.pyplot as plt
 
-def UPW_SPM(size, time, ds, dt,ntag,filename):
+def UPW_SPM(size, time, ds, dt):
 
 # initial conditions -- eventually will move to main file
     # growth rate
@@ -26,9 +26,12 @@ def UPW_SPM(size, time, ds, dt,ntag,filename):
     N = np.zeros([len(time),len(size)])
     N[0,:] = np.exp(-(size[:]-5)**2)
 
+    #analytical solution 
+    sol = np.zeros([len(time),len(size)])
+
 
 # WITH gp - Steve's time splitting  on u_t + (g * u)_s = -mu * u
-    for t in range(0, len(time) -1):
+    for t in range(0, len(time) - 1):
 
         for s in range(1,len(size)): 
             Ntemp = 0
@@ -42,21 +45,42 @@ def UPW_SPM(size, time, ds, dt,ntag,filename):
             # step 3 -- half time step
             N[t+1,s] = Ntemp * np.exp(mu[s] * dt/2)
 
-# Total Population - this is to check if the solution is conservative when we have no death or births
-    total_population_initial = np.sum(N[0, :])
-    total_population_middle = np.sum(N[int((len(time)-1)/2), :])
-    total_population_final = np.sum(N[len(time) -1, :])
 
-    print('total population at t = 0 : ' + str(total_population_initial))
-    print('total population at t = '+ str(int(len(time)/2)) +' : ' + str(total_population_middle))
-    print('total population at t = ' + str(len(time)) + ' : ' + str(total_population_final))
+    for t in range(0, len(time)):
+        for s in range(0, len(size)):
+            # Calculate the analytical solution
+            c = 1/10
+            Y = 1/c * np.log(np.exp(size[s]*c) - time[t]*c)  # g(s) = exp(-s/10)
+            phi = np.exp(-((Y - 5) / 1) ** 2)
+            sol[t,s] = phi * np.exp( c**2 * (Y -  np.log( c*time[t] + np.exp(Y) ) ) ) # g(s) = exp(-s/10), mu(s) = 0
+
+
+
+# # WITH gp - on the board time splitting
+#     for t in range(0, len(time) - 1):
+
+#         for s in range(1,len(size)): 
+#             Ntemp = 0
+
+#             # step 1 -- half time step
+#             Ntemp = N[t,s] * np.exp( ( - mu[s] - gp[s] ) * dt / 2 )
+
+#             # step 2 -- time step 
+#             Ntemp = Ntemp - dt * ( g[s] * ( Ntemp - N[t,s-1] ) / ds ) 
+
+#             # step 3 -- half time step
+#             N[t+1,s] = Ntemp * np.exp( ( - mu[s] - gp[s] ) * dt / 2 )
+
 
 
 # Plots the numerical solution at multiple points in time
     for t_index, N_t in enumerate(N):
-        # plt.plot(size, N_t, label=f'Numerical at time {t_index}', linestyle='--')
         if t_index % 100 == 0:  # Plot only if time index is a multiple of 10
             plt.plot(size, N_t, label=f'Numerical at time {t_index * dt}', linestyle='--')
+    
+    # for t_index, sol_t in enumerate(sol):
+    #     if t_index % 100 == 0:  # Plot only if time index is a multiple of 10
+    #         plt.plot(size, sol_t, label=f'Analytical at time {t_index * dt}', linestyle='-')
 
     plt.axhline(y=1, color='r', linestyle='--', label='y=1')
     # plt.plot(size, N[3], label='Numerical', linestyle='--')
@@ -67,23 +91,62 @@ def UPW_SPM(size, time, ds, dt,ntag,filename):
     plt.legend()
     # plt.ylim(-.2, 1.4)  # Set y-axis limits from 0 to 12
     plt.show()
-
-
     plt.plot()
 
+
+# MOOOVVVE TO CONSERVATION FILE
+# check conservation -- need to have ds * sum
+    # total_population = np.zeros([len(time)])
+    # total_population = size[-1] * np.sum(N, axis=1)
+    # # max_population_index = np.argmax(total_population)
+    # # print(max_population_index)
+
+    # # print(total_population)
+
+
+
+    # # Plot total population over time
+    # plt.plot(time, total_population)
+    # plt.xlabel('Time')
+    # plt.ylabel('Total Population')
+    # plt.title('Total Population over Time')
+    # # plt.grid(True)
+    # plt.show()
+
+    totalPop_num = np.zeros([len(time)])
+    totalPop_sol = np.zeros([len(time)])
+
+    for t in range(len(time)):
+        totalPop_num[t] = ds * np.sum(N[t,:])
+        totalPop_sol[t] = ds * np.sum(sol[t,:])
+
+    error = np.abs(( totalPop_num - totalPop_sol)) 
+    print(error)
+    print('numerical t end : ' + str(totalPop_num[-1]))
+    print('analytical t end : ' + str(totalPop_sol[-1]))
+
+    # Plot total population over time
+    plt.plot(time, error)
+    plt.xlabel('Time')
+    plt.ylabel('Absolute Error |numerical - true solution|')
+    plt.title('Error of Total Population over Time')
+    # plt.grid(True)
+    plt.show()
+
+    plt.plot(time, totalPop_num)
+    plt.xlabel('Time')
+    plt.ylabel('total pop')
+    plt.title('Total Population over Time')
+    # plt.grid(True)
+    plt.show()
+
     return N
-
-
-
-
-
-   # print(np.shape(N))
-    # print(gp)
-    # print(Ntime)
-    # print(size)
-    # print(np.shape(size))
-    # print(np.shape(time))
     
+
+
+
+
+
 
 
 # WITH gp - on the board time splitting
@@ -94,7 +157,6 @@ def UPW_SPM(size, time, ds, dt,ntag,filename):
 
     #         # step 1 -- half time step
     #         Ntemp = N[t,s] * np.exp( ( -mu[s] - gp[s] )* dt/2 )
-
 
     #         # step 2 -- time step 
     #         Ntemp = N[t,s] - dt * ( g[s] * (N[t,s] - N[t,s-1])/ds ) 
@@ -116,3 +178,15 @@ def UPW_SPM(size, time, ds, dt,ntag,filename):
 
 #             # step 3 -- half time step
 #             N[t+1,s] = Ntemp * np.exp(mu[s] * dt/2)
+
+
+
+# # Total Population - this is to check if the solution is conservative when we have no death or births
+#     total_population_initial = np.sum(N[0, :])
+#     total_population_middle = np.sum(N[int((len(time)-1)/2), :])
+#     total_population_final = np.sum(N[len(time) -1, :])
+
+#     print('total population at t = 0 : ' + str(total_population_initial))
+#     print('total population at t = '+ str(int(len(time)/2)) +' : ' + str(total_population_middle))
+#     print('total population at t = ' + str(len(time)) + ' : ' + str(total_population_final))
+

main.py
@@ -10,26 +10,44 @@ Smax = 15
 Tmax = 2
 
 dt = 0.01
-# ds = np.zeros([1])
+# ds = np.zeros([2])
 # ds[0] = 0.9 * dt
+# ds[1] = 0.9 * dt / 2
 ds = 0.9 * dt
-Ntest = 0
+# Ntest =  range(len(ds))
 filename = 'test'
 
 # initalize arrays
 Nsize = int(Smax/ds) + 1
 Ntime = int(Tmax/dt) + 1
-size = np.linspace(0,Smax, Nsize)
+size = np.linspace(0,Smax, Nsize) # analytical solutions exist starting at 0.7
 time = np.linspace(0,Tmax, Ntime)
 
 data = np.zeros([Ntime,Nsize])
 
-with open(filename + 'up_' + str(Ntest) + '.txt', 'w') as file: # Initialise an outputter file (safe)
+data = UPW_SPM(size, time, ds, dt)
 
-    data = UPW_SPM(size, time, ds, dt, Ntest, filename)
 
-convergence_ds(size, time, ds, dt, Ntest, filename, data)
 
+# for i in 1:
+
+#     print('Entering loop ' + str(i))
+
+#     data = UPW_SPM(size, time, ds, dt)
+
+#     np.savetxt(filename + 'upwind_num_' + str(i) + '.txt', data)
+
+#     print('S Loop ' + str(i) + ' Complete.')
+
+    # with open(filename + 'upwind_num_' + str(i) + '.txt', 'w') as file: # Initialise an outputter file (safe)
+
+
+
+
+
+
+# print(np.shape(data))
+# convergence_ds(size, time, ds, dt, Ntest, filename, data)
 
 
 # filename = 'ds_convergence/' # Folder name for data storage.